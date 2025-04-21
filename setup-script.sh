fda090eba6fc261038c628654f83a40dd11c5450
yahyatoumi <80495767+yahyatoumi@users.noreply.github.com>
2025-04-21 09:50:11 +0100

    Fresh repo

---
+++ setup-script.sh
@@ -0,0 +1,57 @@
+# SERVER_IP="192.168.56.110"
+
+
+# # Install Docker
+# apt-get update
+# apt-get install -y ca-certificates curl gnupg git avahi-daemon libnss-mdns
+# install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# chmod a+r /etc/apt/keyrings/docker.gpg
+# echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# usermod -aG docker vagrant
+
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip $SERVER_IP --tls-san $SERVER_IP" sh -
+
+# # Detect architecture
+# ARCH=$(uname -m)
+# if [[ "$ARCH" == "x86_64" ]]; then
+#   ARCH="amd64"
+# elif [[ "$ARCH" == "aarch64" ]]; then
+#   ARCH="arm64"
+# fi
+
+# # Download correct kubectl binary and checksum
+# KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+
+# curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/${ARCH}/kubectl"
+# curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/${ARCH}/kubectl.sha256"
+
+# # Verify checksum
+# echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+# # Install kubectl
+# # chmod +x kubectl
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# mkdir -p /home/vagrant/.kube
+# sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+# sudo chown vagrant:vagrant /home/vagrant/.kube/config
+# sed -i "s/127.0.0.1/${SERVER_IP}/g" /home/vagrant/.kube/config
+
+# # Verify kubectl
+# kubectl version --client --output=yaml
+
+# curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+k3d cluster create mycluster
+k3d kubeconfig write mycluster
+export KUBECONFIG=$(k3d kubeconfig write mycluster)
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+kubectl create namespace dev
+
+
