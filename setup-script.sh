12dc6a1308a03f9cc1b49c74ddee7aa47e438991
yahyatoumi <80495767+yahyatoumi@users.noreply.github.com>
2025-05-22 16:28:02 +0100

    Using Will42 docker image instead

--- setup-script.sh
+++ setup-script.sh
@@ -1,13 +1,18 @@
 SERVER_IP="192.168.56.110"
 
-
-# Install Docker
+# Add Docker's official GPG key:
 apt-get update
-apt-get install -y ca-certificates curl gnupg git avahi-daemon libnss-mdns
+apt-get install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 usermod -aG docker vagrant
 
@@ -31,7 +36,7 @@ curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/${ARCH}/kubectl
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 # Install kubectl
-# chmod +x kubectl
+chmod +x kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Verify kubectl
@@ -39,17 +44,17 @@ kubectl version --client --output=yaml
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-# k3d cluster create mycluster -p "80:80@loadbalancer"
-# k3d kubeconfig write mycluster
-# export KUBECONFIG=$(k3d kubeconfig write mycluster)
+k3d cluster create mycluster -p "80:80@loadbalancer"
+k3d kubeconfig write mycluster
+export KUBECONFIG=$(k3d kubeconfig write mycluster)
 
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
-# kubectl create namespace dev
-# kubectl get ns
-# kubectl config current-context
+kubectl create namespace dev
+kubectl get ns
+kubectl config current-context
 
 VERSION="v2.10.2"
 echo "Using ArgoCD version: $VERSION"
@@ -57,6 +62,8 @@ echo "Using ArgoCD version: $VERSION"
 curl -s https://api.github.com/repos/argoproj/argo-cd/releases/tags/$VERSION | grep "browser_download_url.*arm"
 
 curl -L -o argocd-linux-arm64 https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-arm64
-file argocd-linux-arm64
+# file argocd-linux-arm64
 chmod +x argocd-linux-arm64
 sudo mv argocd-linux-arm64 /usr/local/bin/argocd
+
+# QOfutA-jO3VKYJYf
